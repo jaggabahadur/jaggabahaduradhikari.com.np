31be819652feeffe9250ff279b9fb7e3497d565a
jaggabahadur <jagatjumla06@gmail.com>
2026-07-17 11:03:40 +0545

    Update admin.js

--- admin.js
+++ admin.js
@@ -4,3 +4,41 @@ document.getElementById("msg").innerHTML =
 "Login system will be connected in the next step.";
 
 }
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  const firebaseConfig = {
+  apiKey: "AIzaSyCqFBAC69apk5PDGtrBX5RkJHQQFl1sLEU",
+  authDomain: "jagga-website.firebaseapp.com",
+  projectId: "jagga-website",
+  storageBucket: "jagga-website.firebasestorage.app",
+  messagingSenderId: "823930371338",
+  appId: "1:823930371338:web:670961a40b66d6b9a1cef3"
+};
+  authDomain: "jagga-website.firebaseapp.com",
+  projectId: "jagga-website",
+  storageBucket: "jagga-website.firebasestorage.app",
+  messagingSenderId: "823930371338",
+  appId: "1:823930371338:web:670961a40b66d6b9a1cef3"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Login Successful!");
+      window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+      document.getElementById("msg").innerHTML = error.message;
+    });
+};
