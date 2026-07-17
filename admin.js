aab2fa06fe4f5af23235f87ce462ce50af4650e3
jaggabahadur <jagatjumla06@gmail.com>
2026-07-17 11:33:51 +0545

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,25 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-const firebaseConfig = {
-  apiKey: "AIzaSyCqFBAC69apk5PDGtrBX5RkJHQQFl1sLEU",
-  authDomain: "jagga-website.firebaseapp.com",
-  projectId: "jagga-website",
-  storageBucket: "jagga-website.firebasestorage.app",
-  messagingSenderId: "823930371338",
-  appId: "1:823930371338:web:670961a40b66d6b9a1cef3"
-};  getAuth,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  const firebaseConfig = {
   apiKey: "AIzaSyCqFBAC69apk5PDGtrBX5RkJHQQFl1sLEU",
-  authDomain: "jagga-website.firebaseapp.com",
-  projectId: "jagga-website",
-  storageBucket: "jagga-website.firebasestorage.app",
-  messagingSenderId: "823930371338",
-  appId: "1:823930371338:web:670961a40b66d6b9a1cef3"
-};
   authDomain: "jagga-website.firebaseapp.com",
   projectId: "jagga-website",
   storageBucket: "jagga-website.firebasestorage.app",
@@ -35,11 +18,13 @@ window.login = function () {
   const password = document.getElementById("password").value;
 
   signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
+    .then((userCredential) => {
       alert("Login Successful!");
       window.location.href = "dashboard.html";
     })
     .catch((error) => {
       document.getElementById("msg").innerHTML = error.message;
     });
+
+  return false;
 };
